acad265ae7cfe4a7e553aebef02df7cc89a076ee
jstar1 <jusmoore@pdx.edu>
2016-07-08 10:05:23 -0700

    Creating the player class

---
+++ src/player.h
@@ -0,0 +1,38 @@
+/*
+------------------------
+   Uno Card Game
+   player.h file
+------------------------
+ */
+ 
+
+/*
+-----------------------
+ Define Headee
+-----------------------
+*/
+
+#ifdef PLAYER__H
+#define PLAYER__H
+
+/*
+-----------------------
+Include files
+-----------------------
+*/
+
+#include <vector>
+#include "card.h"
+
+
+/*
+----------------------
+Class Player
+----------------------
+*/
+
+class Player
+{
+	public:
+	private:
+};
